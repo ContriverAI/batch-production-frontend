7509967e876f4d6cf4abfc73e9d4707e5c889c7b
kushal agrawal <kagrawal61@gmail.com>
2020-09-11 11:50:18 +0530

    time element formatted

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, request, render_template, url_for
 
 app = Flask(__name__)
 
+@app.route('/superviser_cooling', methods=['GET','POST'])
+def superviser_cooling():
+    return render_template('superviser_cooling.html')
+
 @app.route('/user_cooling', methods=['GET','POST'])
 def user_cooling():
     return render_template('user_cooling.html')
