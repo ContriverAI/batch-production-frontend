9b659a729d8b089adf8a509a491cc000bff0c409
kushal agrawal <kagrawal61@gmail.com>
2020-09-11 11:20:48 +0530

    moved to flask

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/user_cooling', methods=['GET','POST'])
+def user_cooling():
+    return render_template('user_cooling.html')
+
+@app.route('/', methods=['GET','POST'])
+def login():
+    return render_template('index.html')
+
+app.run(host='0.0.0.0', port=9001)
